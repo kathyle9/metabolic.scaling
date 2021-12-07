d9a68ba400a422ed014de4f1b26686c2ea7d4c9f
Kathy Le <kathyle@stanford.edu>
2021-12-07 10:59:20 -0800

    load_data outputs only relevant columns

--- R/load_data.R
+++ R/load_data.R
@@ -1,15 +1,17 @@
 #' Load raw data
 #'
-#' This turns the raw data Excel file into a dataframe.
+#' This turns the raw data Excel file into a dataframe. Removes first 5 rows.
 #'
-#' @param filename
+#' @param filename: path to raw data file
 #'
-#' @return
+#' @return dataframe with 4 columns
 #' @export
 #' @examples
 #' load_data("rawdata.xls")
 load_data <- function(filename) {
-  readxl::read_excel(filename)
+  df <- readxl::read_excel(filename)
+  df <- df[-c(1:5),c(4,7,8,10)]
+  df
 }
 
 
