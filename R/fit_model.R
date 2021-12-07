d39b4a35e426236c25ec0c53ae746ab06b482798
Joy Doong <joydoong@pop-os.localdomain>
2021-12-07 11:42:16 -0800

    Add fit_model

---
+++ R/fit_model.R
@@ -0,0 +1,17 @@
+#' Fit data to linear or quadratic model
+#'
+#' @param df A dataframe containing species name, mass, metabolic rates, body temperature
+#' @param degree An integer which is the degree of polynomial: 1 (default)
+#' @param temp_correct A flag for temperature correction, TRUE or FALSE (default)
+#' @param exclusion A list of species name to exclude
+#' @return The coefficients for the fitted model
+
+fit_model <- function(df, degree=1, temp_correct=FALSE, exclusion=list()) {
+  df_sub <- subset(df, !df$Species %in% exclusion)
+  if (temp_correct){
+    model <- lm(get("BMR (W)") ~ poly(get("Mass (g)"), degree=degree) + I(1/get("Temperature (C)")), data = df_sub)
+  } else {
+    model <- lm(get("BMR (W)") ~ poly(get("Mass (g)"), degree=degree), data = df_sub)
+  }
+  return(model$coefficients)
+}
