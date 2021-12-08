8af95c7863957d8974e41f22797f8e13ac7ad988
Joy Doong <joydoong@pop-os.localdomain>
2021-12-08 10:46:25 -0800

    Updated model_prediction.R

--- R/fit_model.R
+++ R/fit_model.R
@@ -9,9 +9,9 @@
 fit_model <- function(df, degree=1, temp_correct=FALSE, exclusion=list()) {
   df_sub <- subset(df, !df$Species %in% exclusion)
   if (temp_correct){
-    model <- lm(get("BMR (W)") ~ poly(get("Mass (g)"), degree=degree) + I(1/get("Temperature (C)")), data = df_sub)
+    model <- lm(get("BMR (W)") ~ poly(get("Mass (g)"), degree=degree, raw=TRUE) + I(1/get("Temperature (C)")), data = df_sub)
   } else {
-    model <- lm(get("BMR (W)") ~ poly(get("Mass (g)"), degree=degree), data = df_sub)
+    model <- lm(get("BMR (W)") ~ poly(get("Mass (g)"), degree=degree, raw=TRUE), data = df_sub)
   }
   return(model)
 }
