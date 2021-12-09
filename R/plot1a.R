dde25da96ff7c83623f34d8276f6d3baf1544205
Kathy Le <kathyle@stanford.edu>
2021-12-09 05:50:55 -0800

    make pretty

--- R/plot1a.R
+++ R/plot1a.R
@@ -1,5 +1,6 @@
 plot1a <- function(df) {
-  data_points = ggplot2::ggplot(df, ggplot2::aes(x=`Mass (g)`, y=`BMR (W)`)) + ggplot2::geom_point()
-  linear = ggplot2::geom_line(ggplot2::aes(x=`Mass (g)`, y=linear_predict))
-  data_points + linear
+  data_points = ggplot2::ggplot(df, ggplot2::aes(x=`Mass (g)`, y=`BMR (W)`)) + ggplot2::geom_point(size = 0.5)
+  linear = ggplot2::geom_line(ggplot2::aes(x=`Mass (g)`, y=linear_predict, color = "red"))
+  quadratic = ggplot2::geom_line(ggplot2::aes(x=`Mass (g)`, y=quadratic_predict, color = "blue"))
+  data_points + linear + quadratic + ggplot2::xlab('Log10[M (g)]') + ggplot2::ylab('Log10[BMR (W)]')
 }
