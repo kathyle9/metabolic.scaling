09844520ec907832a0c494640b8725cf2ddc09a0
Kathy Le <kathyle@stanford.edu>
2021-12-09 05:01:55 -0800

    progress

--- R/plot1a.R
+++ R/plot1a.R
@@ -1,5 +1,5 @@
 plot1a <- function(df) {
-  data_points = ggplot(df, aes(x=`Mass (g)`, y=`BMR (W)`)) + geom_point()
-  linear = geom_line(aes(x=`Mass (g)`, y=linear_predict))
+  data_points = ggplot2::ggplot(df, ggplot2::aes(x=`Mass (g)`, y=`BMR (W)`)) + ggplot2::geom_point()
+  linear = ggplot2::geom_line(ggplot2::aes(x=`Mass (g)`, y=linear_predict))
   data_points + linear
 }
