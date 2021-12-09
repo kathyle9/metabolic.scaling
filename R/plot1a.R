f91b589c3422e5918aa3f43eddbd18141f54bc5c
Joy Doong <joydoong@pop-os.localdomain>
2021-12-09 11:59:32 -0800

    Update plot1b

--- R/plot1a.R
+++ R/plot1a.R
@@ -6,8 +6,13 @@
 #' @export
 #'
 plot1a <- function(df) {
-  data_points = ggplot(df, aes(x=`Mass (g)`, y=`BMR (W)`)) + geom_point(size = 0.5)
+  data_points = ggplot(df, aes(x=`Mass (g)`, y=`BMR (W)`)) +
+    geom_point(size = 0.5)
   linear = geom_line(aes(x=`Mass (g)`, y=linear_predict), color='red3')
   quadratic = geom_line(aes(x=`Mass (g)`, y=quadratic_predict), color='blue3')
-  data_points + linear + quadratic + xlab('Log10[M (g)]') + ylab('Log10[BMR (W)]')
+  data_points +
+    linear +
+    quadratic +
+    xlab('Log10[M (g)]') +
+    ylab('Log10[BMR (W)]')
 }
--- R/plot1b.R
+++ R/plot1b.R
@@ -14,8 +14,27 @@ plot1b <- function(x_range, linear_lm, quadratic_lm, hline=c(2/3, 3/4)){
   data <- data.frame(logmass = x_range,
                      quadratic_slope = coefs[2] + 2*x_range*coefs[3])
 
-  g <- ggplot(data, aes(x=logmass, y=quadratic_slope)) +
-    geom_line() +
-    geom_hline(yintercept = linear_lm$coefficients[2])
+  g <- ggplot(data, aes(x = logmass,
+                        y = quadratic_slope)) +
+    geom_line(aes(color = "Quadratic")) +
+    geom_hline(aes(color="Power law"), yintercept = linear_lm$coefficients[2],
+               color = 'red') +
+    geom_hline(yintercept = hline, aes(color = '2/3 and 3/4')) +
+    labs(y="Slope", x=expression(paste("log"[10], "[M (g)]"))) +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          legend.position = c(0,1),
+          legend.justification = c(0,1),
+          legend.background = element_rect(fill=NA),
+          legend.title = element_blank()) +
+    scale_color_manual(values = c('2/3 and 3/4' = 'black',
+                                'Power law' = 'red',
+                                'Quadratic' = 'blue')) +
+    coord_cartesian(expand = FALSE) +
+    scale_y_continuous(breaks = seq(round(min(data$quadratic_slope), digits = -2),
+                                    max(data$quadratic_slope),
+                                    0.05))
+
   g
 }
