f28b4c3a204b6fae53c4f2824c82d1a2a1dcdd69
Kathy Le <kathyle@stanford.edu>
2021-12-09 06:35:15 -0800

    color lines

--- R/plot1a.R
+++ R/plot1a.R
@@ -1,6 +1,6 @@
 plot1a <- function(df) {
   data_points = ggplot2::ggplot(df, ggplot2::aes(x=`Mass (g)`, y=`BMR (W)`)) + ggplot2::geom_point(size = 0.5)
-  linear = ggplot2::geom_line(ggplot2::aes(x=`Mass (g)`, y=linear_predict, color = "red"))
-  quadratic = ggplot2::geom_line(ggplot2::aes(x=`Mass (g)`, y=quadratic_predict, color = "blue"))
+  linear = ggplot2::geom_line(ggplot2::aes(x=`Mass (g)`, y=linear_predict), color='red3')
+  quadratic = ggplot2::geom_line(ggplot2::aes(x=`Mass (g)`, y=quadratic_predict), color='blue3')
   data_points + linear + quadratic + ggplot2::xlab('Log10[M (g)]') + ggplot2::ylab('Log10[BMR (W)]')
 }
