055d197f7ebf3f871c319ce2b69e7dba446cb3f0
Kathy Le <kathyle@stanford.edu>
2021-12-09 09:27:01 -0800

    plot1b

--- R/plot1b.R
+++ R/plot1b.R
@@ -2,7 +2,7 @@ plot1b <- function(x_range, linear_lm, quadratic_lm, hline=c(2/3, 3/4)){
   coefs <- quadratic_lm$coefficients
   #coefs <- coefs[which(grepl("Mass", names(coefs)))]
   data <- data.frame(logmass = x_range,
-                     quadratic_slope = coefs[2] + x_range*coefs[3])
+                     quadratic_slope = coefs[2] + 2*x_range*coefs[3])
 
   g <- ggplot2::ggplot(data, ggplot2::aes(x=logmass, y=quadratic_slope)) +
     ggplot2::geom_line() +
