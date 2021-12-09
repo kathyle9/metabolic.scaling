4f1dd3f32db3f8f922fc31e73f29b432ae2c4221
Kathy Le <kathyle@stanford.edu>
2021-12-09 11:25:08 -0800

    plot1a done

--- R/plot1a.R
+++ R/plot1a.R
@@ -7,7 +7,11 @@
 #' @export
 plot1a <- function(df) {
   data_points = ggplot(df, aes(x=`Mass (g)`, y=`BMR (W)`)) + geom_point(size = 0.5)
-  linear = geom_line(aes(x=`Mass (g)`, y=linear_predict), color='red3')
-  quadratic = geom_line(aes(x=`Mass (g)`, y=quadratic_predict), color='blue3')
-  data_points + linear + quadratic + xlab('Log10[M (g)]') + ylab('Log10[BMR (W)]')
+  linear = geom_line(aes(x=`Mass (g)`, y=linear_predict, color='linear'))
+  quadratic = geom_line(aes(x=`Mass (g)`, y=quadratic_predict, color='quadratic'))
+  orca_point = geom_point(aes(color='Orca'), data = df[which.max(df$`Mass (g)`), ], size=1, shape=15)
+  data_points + linear + quadratic + orca_point + xlab('Log10[M (g)]') + ylab('Log10[BMR (W)]') +
+    scale_color_manual(values = c('Orca'='green3', 'linear'='red2', 'quadratic'='blue2'),
+                       guide = guide_legend(override.aes = list(linetype = c(1,0,1)))) +
+    theme(legend.title = element_blank())
 }
