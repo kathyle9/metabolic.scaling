0700f3ba9ffd3fd07421ada37728e9a38e4bdc03
Kathy Le <kathyle@stanford.edu>
2021-12-09 11:41:20 -0800

    pretty plot1a

--- R/plot1a.R
+++ R/plot1a.R
@@ -10,8 +10,12 @@ plot1a <- function(df) {
   linear = geom_line(aes(x=`Mass (g)`, y=linear_predict, color='linear'))
   quadratic = geom_line(aes(x=`Mass (g)`, y=quadratic_predict, color='quadratic'))
   orca_point = geom_point(aes(color='Orca'), data = df[which.max(df$`Mass (g)`), ], size=1, shape=15)
-  data_points + linear + quadratic + orca_point + xlab('Log10[M (g)]') + ylab('Log10[BMR (W)]') +
+  data_points + linear + quadratic + orca_point + xlab(expression(paste("log"[10]," M (g)"))) + ylab(expression(paste("log"[10]," BMR (W)"))) +
     scale_color_manual(values = c('Orca'='green3', 'linear'='red2', 'quadratic'='blue2'),
                        guide = guide_legend(override.aes = list(linetype = c(1,0,1)))) +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          legend.position = c(0,1), legend.justification = c(0,1),
+          legend.background = element_rect(fill=NA)) +
     theme(legend.title = element_blank())
 }
