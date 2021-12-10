17ba9e9c60e5985224cc6a369315c7e9a4c959dd
Kathy Le <kathyle@stanford.edu>
2021-12-10 10:06:43 -0800

    two smoke test, testing correct class

--- tests/testthat/test-smoke.R
+++ tests/testthat/test-smoke.R
@@ -1,3 +1,8 @@
-test_that("fit_model works", {
-  fit_model(data, degree=1)
+test_that("fit_model is lm class", {
+  expect_s3_class(fit_model(metabolic, degree=1), "lm")
 })
+
+test_that("plot1a is gg class", {
+  expect_s3_class(plot1a(metabolic), "gg")
+})
+
